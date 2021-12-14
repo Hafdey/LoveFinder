7b1cca0812a1976c917664d4d483f29710783a34
Hafdey <brandonwijnands@live.nl>
2021-12-14 13:35:36 +0100

    android db info

--- LoveFinder/LoveFinder.Android/MainActivity.cs
+++ LoveFinder/LoveFinder.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using System.IO;
 
 namespace LoveFinder.Droid
 {
@@ -16,7 +17,11 @@ namespace LoveFinder.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+
+            string dbName = "appdatabase.sqllite";
+            string folderPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            string fullPath = Path.Combine(folderPath, dbName);
+            LoadApplication(new App(fullPath));
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
